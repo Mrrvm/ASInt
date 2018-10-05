84ea831d6e0d6d063b8964069de40a44fd02b46c
carlosaleluia <32376486+carlosaleluia@users.noreply.github.com>
2018-10-05 12:44:55 +0100

    server, client working with rpnCalculator

--- lab2/client.py
+++ lab2/client.py
@@ -8,15 +8,17 @@ port = 4120
 
 s.connect((host, port))
 
-s.send('push')
-s.send(str(input('1st number: ')))
+s.send('push'.encode())
+s.send(str(input('1st number: ')).encode())
 
-s.send('push')
-s.send(str(input('2nd number: ')))
+s.send('push'.encode())
+s.send(str(input('2nd number: ')).encode())
 
-s.send('push')
-s.send(str(input('3rd number: ')))
+s.send('push'.encode())
+s.send(str(input('3rd number: ')).encode())
 
-s.send('addsub')
+s.send('addsub'.encode())
+
+s.send('close'.encode())
 
 s.close();
--- lab2/server.py
+++ lab2/server.py
@@ -1,5 +1,5 @@
 from socket import *
-
+import lab2_module
 
 s = socket(AF_INET, SOCK_STREAM)
 
@@ -12,22 +12,22 @@ s.bind((host, port))
 s.listen(5)
 
 c, addr = s.accept()
-
-print 'Connection from', addr
+Obj = lab2_module.rpnCalculator()
+print('Connection from')
+print(addr)
 
 while True:
-	
-	ret.clear()
-	ret = c.recv(32)
-	if ret == 'push':
-		ret.clear()
-		ret = c.recv(32)
-		print (ret)
+    ret = c.recv(32).decode()
+    print(ret)
+    if ret == 'push':
+        ret = c.recv(32).decode()
+        Obj.pushValue(int(ret))
 
-	elif ret == 'addsub':
-		ret.clear()
-		ret = c.recv(32)
-		print ('hello')
+    elif ret == 'addsub':
+        Obj.AddSub()
 
+    elif ret == 'close':
+        break
 
+Obj.print()
 c.close()
